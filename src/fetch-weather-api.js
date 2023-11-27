118eff7360c8b61ba251e3c25469d1b3887fa3ea
enduraew <enduraew@gmail.com>
2023-11-27 16:49:26 +0500

    Change to https

--- src/fetch-weather-api.js
+++ src/fetch-weather-api.js
@@ -17,7 +17,7 @@ export function getDatas(response) {
 export default async function fetchAPI(location) {
   try {
     const API_KEY = 'e30f31669c774a29bd871601232411';
-    const getPromise = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`, {
+    const getPromise = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`, {
       mode: 'cors'
     });
     const response = await getPromise.json();
